a025858cbbebb57598bdb5df20b9d4e5e43d94e9
Joe Lau <227020101@stu.vtc.edu.hk>
2023-05-04 23:50:30 +0800

    Complete Recommend Hashtag function

--- app/src/main/java/edu/shape/postdraftsforsocialmedia/MainActivity.kt
+++ app/src/main/java/edu/shape/postdraftsforsocialmedia/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.net.Uri
@@ -12,17 +13,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.RequiresApi
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import java.io.File
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    lateinit var image_holder: ImageView
+    lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        image_holder = findViewById(R.id.imageView)
+        image_holder.setImageResource(R.drawable.ic_launcher_foreground)
+        editText = findViewById(R.id.editTextHashtag)
+
     }
 
     companion object {
@@ -40,15 +48,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val imageView = findViewById<ImageView>(R.id.imageView)
         if(requestCode == REQUEST_SELECT_IMAGE && resultCode == RESULT_OK) {
             val selectedImageUri = data?.data as Uri
-            imageView.setImageURI(selectedImageUri)
+            image_holder.setImageURI(selectedImageUri)
         } else if (requestCode == REQUEST_IMAGE_CATPURE && resultCode == RESULT_OK) {
             val bitmap = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(bitmap)
+            image_holder.setImageBitmap(bitmap)
         }
+        generateLabels()
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun generateLabels() {
+        val bitmap = (image_holder!!.drawable as BitmapDrawable).bitmap
+        val image = InputImage.fromBitmap(bitmap, 0)
+        val laber = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+        laber.process(image).addOnSuccessListener { labels ->
+            var labelText = "Recommend Hashtag"
+            for (label in labels) {
+                val text = "#"+label.text
+                labelText += "\n$text"
+            }
+            val finalLabelText = labelText
+            runOnUiThread { editText.setText(finalLabelText) }
+        }
     }
 
     var mIsRecording = false
